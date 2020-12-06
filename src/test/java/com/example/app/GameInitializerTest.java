7ae69eafc4c8b511fc28e4c9b6f51fdaa8c34b66
otouski <otouskii@gmail.com>
2020-12-06 14:01:54 +0100

    Implemented test methods to dynamically initialize the game grid with n rows and cells

--- src/test/java/com/example/app/GameInitializerTest.java
+++ src/test/java/com/example/app/GameInitializerTest.java
@@ -53,6 +53,65 @@ public class GameInitializerTest {
 		
 	}
 
+	/**
+	 * This test method is for dynamic initialization of the game grid to 1 rows of n cells(cols)
+	 * The test passes if the number of cells(cols) is 3
+	 */
+	@Test
+	public void testCreateGridColCellWithDynamicNumber() {
+
+		// Declare and initialize Map variable that represent the game grid to hold played value
+		Map<String,List<String>> grid=new HashMap<String, List<String>>();
+
+		// Declare and initialize int variable that represent the number of cells(cols). Here variable set to 3
+		int cols=3;
+
+		// Declare and initialize List variable that represent the row of the game grid. Size of the list is 3 (3 cells)
+		List<String> cellsA=new ArrayList<String>(cols);
+		for(int i=0; i<cols;i++) {
+			cellsA.add("");
+		}
+		grid.put("A", cellsA);
+
+		// if cellsA.size() is 3, assetEquals is true. The test passes
+		// else the test failed
+		assertEquals(3, cellsA.size());
+
+
+	}
+
+
+	/**
+	 * This test method is for dynamic initialization of the game grid to n rows of n cells(cols)
+	 * The test passes if the map variable "grid" size is 1 (number of rows in the game grid)
+	 *
+	 */
+	@Test
+	public void testCreateGridRowCellWithDynamicNumber() {
+
+		// Declare and initialize Map variable that represent the game grid to hold played value
+		Map<String,List<String>> grid=new HashMap<String, List<String>>();
+
+		// Declare and initialize int variable that represent the number of cells(cols). Here variable set to 3
+		int cols=3;
+
+		// Declare and initialize int variable that represent the number of rows. Here variable set to 3
+		int row=3;
+
+		// Declare and initialize List variable that represent the row of the game grid. Size of the list is 3 (3 cells)
+		List<String> cellsA=new ArrayList<String>(cols);
+		for(int i=0; i<cols;i++) {
+			cellsA.add("");
+		}
+		grid.put(String.valueOf(row), cellsA);
+
+		// if grid.size() is 1, assetEquals is true. The test passes
+		// else the test failed
+		assertEquals(1, grid.size());
+
+
+	}
+
 
 
 
